be3924ec774cf16fd67e57876d46379fb7d626f0
MasterYi <>
2019-07-12 15:39:09 +0800

    update

---
+++ InfiteeAnimation/InfiteeAnimation/Module/个人中心/INFMineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  INFMineViewController.swift
+//  InfiteeAnimation
+//
+//  Created by MasterYi on 2019/7/12.
+//  Copyright © 2019 Master.Yi. All rights reserved.
+//
+
+import UIKit
+
+class INFMineViewController: INFBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InfiteeAnimation/InfiteeAnimation/View/INFMineMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  INFMineMessageCell.swift
+//  InfiteeAnimation
+//
+//  Created by MasterYi on 2019/7/12.
+//  Copyright © 2019 Master.Yi. All rights reserved.
+//
+
+import UIKit
+
+class INFMineMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
