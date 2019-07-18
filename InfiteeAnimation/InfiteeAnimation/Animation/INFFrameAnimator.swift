cb5eb4c0ec6d9484ca19f12d94b7a66aeb696397
MasterYi <>
2019-07-18 09:09:07 +0800

    update

---
+++ InfiteeAnimation/InfiteeAnimation/Animation/INFFrameAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  INFFrameAnimator.swift
+//  InfiteeAnimation
+//
+//  Created by MasterYi on 2019/7/15.
+//  Copyright © 2019 Master.Yi. All rights reserved.
+//
+
+import UIKit
+
+class INFFrameAnimator: INFTransitionAnimator {
+
+}
---
+++ InfiteeAnimation/InfiteeAnimation/View/INFMineHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  INFMineHeaderView.swift
+//  InfiteeAnimation
+//
+//  Created by MasterYi on 2019/7/15.
+//  Copyright © 2019 Master.Yi. All rights reserved.
+//
+
+import UIKit
+
+class INFMineHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
